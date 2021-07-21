27b236f866301201a997d6f76ab009b036629d2b
AnaC-Sousa <ana.catarina.s@hotmail.com>
2021-07-21 18:51:18 +0100

    rover goes in front

--- pluto.rover.api/pluto.rover.api.tests/PlutoRoverTest.cs
+++ pluto.rover.api/pluto.rover.api.tests/PlutoRoverTest.cs
@@ -18,6 +18,29 @@ namespace pluto.rover.api.tests
 
       Assert.IsTrue(pluto.Size.Equals("100;100"));
     }
+
+    [Test]
+    public void InitiateRover_ShouldReturnInitialPosition()
+    {
+      var roverOrders = new RoverOrders();
+
+      var roverInitialized = roverOrders.Initialize();
+
+      Assert.IsTrue(roverInitialized.Position.Equals("0;0") && roverInitialized.Direction.Equals('N'));
+    }
+
+    [Test]
+    public void GoInFront_ShouldRetunOneUnityMoreInNorth()
+    {
+      var roverOrders = new RoverOrders();
+
+      Rover rover = roverOrders.Initialize();
+
+      rover = roverOrders.GoFront();
+
+      Assert.IsTrue(rover.Position.Equals("0;1") && rover.Direction.Equals('N'));
+    }
+
   }
 }
 
---
+++ pluto.rover.api/pluto.rover.api/Entities/Rover.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace pluto.rover.api
+{
+  public class Rover
+  {
+    public string Position { get; set; }
+    public char Direction { get; set; }
+  }
+}
